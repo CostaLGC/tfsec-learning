0967a82328d9c5d185f36caa7244827fccd0a00b
CostaLGC <lg2costa@gmail.com>
2024-05-02 20:53:44 +0200

    Update sec-group.tf

--- sec-group.tf
+++ sec-group.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group_rule" "my-rule" {
-  #description = "Allow inbound HTTP traffic"
+  description = "Allow inbound HTTP traffic"
   type = "ingress"
   cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-ingress-sgr
 }
